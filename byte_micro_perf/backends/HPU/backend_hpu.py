0711169318497ce30d8eb7eb93337e626080b67c
Youlei Yang <youlei.yang@intel.com>
2024-12-11 08:01:21 +0000

    use HPU device API

--- byte_micro_perf/backends/HPU/backend_hpu.py
+++ byte_micro_perf/backends/HPU/backend_hpu.py
@@ -1,4 +1,4 @@
-# Copyright 2023 ByteDance and/or its affiliates.
+# Copyright 2024 ByteDance and/or its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,50 +25,47 @@ import torch.distributed.distributed_c10d as dist_c10d
 
 from backends import module_store
 from backends.backend import Backend
-
+import habana_frameworks.torch as htorch
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("PerfEngine")
 
-
-
-class BackendGPU(Backend):
+class BackendHPU(Backend):
     def __init__(self, workload_dict, vendor_path):
         super().__init__(workload_dict, vendor_path)
 
-        
-
-
     def get_device_name(self):
-        return torch.cuda.get_device_name(0)
+        return torch.hpu.get_device_name()
 
     def get_torch_device_name(self):
-        return "cuda"
+        return "hpu"
 
     def get_device_properties(self):
-        return torch.cuda.get_device_properties(0)
+        return torch.hpu.get_device_properties()
 
     def get_device_count(self):
-        return torch.cuda.device_count()
+        return torch.hpu.device_count()
 
     def set_device(self, device_index : int):
-        torch.cuda.set_device(device_index)
+        torch.hpu.set_device(device_index)
     
     def get_device(self):
-        return torch.cuda.current_device()
+        return torch.hpu.current_device()
     
     def device_synchronize(self):
-        torch.cuda.synchronize()
+        torch.hpu.synchronize()
 
     def empty_cache(self):
-        torch.cuda.empty_cache()
-
-
+        # torch.hpu.empty_cache()
+        raise NotImplementedError("No implementation of empty_cache() on Gaudi")
 
     def get_dist_module(self):
         return dist
 
     def initialize_ccl(self, rank, world_size):
+        # from habana_frameworks.torch.distributed.hccl import initialize_distributed_hpu
+        # world_size, rank, local_rank = initialize_distributed_hpu()
+
         # check device_count
         device_count = self.get_device_count()
         if world_size > device_count:
