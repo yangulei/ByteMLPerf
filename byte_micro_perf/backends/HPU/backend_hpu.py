05af07016604225d28ae914fa28059212916a858
Youlei Yang <youlei.yang@intel.com>
2024-12-13 03:19:09 +0000

    delay the import of habana_frameworks to fix mp issue

--- byte_micro_perf/backends/HPU/backend_hpu.py
+++ byte_micro_perf/backends/HPU/backend_hpu.py
@@ -15,6 +15,7 @@
 import os
 import json
 import logging
+import subprocess
 
 from datetime import timedelta
 from typing import Any, Dict, List
@@ -25,7 +26,6 @@ import torch.distributed.distributed_c10d as dist_c10d
 
 from backends import module_store
 from backends.backend import Backend
-import habana_frameworks.torch as htorch
 
 from dataclasses import dataclass
 
@@ -35,45 +35,66 @@ class HPUDeviceProperties:
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("PerfEngine")
+log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 
 class BackendHPU(Backend):
     def __init__(self, workload_dict, vendor_path):
+        log.debug(f"BackendHPU.__init__() called by pid: {os.getpid()}")
         super().__init__(workload_dict, vendor_path)
 
     def get_device_name(self):
-        return torch.hpu.get_device_name()
+        log.debug(f"BackendHPU.get_device_name() called by pid: {os.getpid()}")
+        # return torch.hpu.get_device_name()
+        return "Gaudi2"
 
     def get_torch_device_name(self):
+        log.debug(f"BackendHPU.get_torch_device_name() called by pid: {os.getpid()}")
+        import habana_frameworks.torch as htorch
         return "hpu"
 
     def get_device_properties(self):
+        log.debug(f"BackendHPU.get_device_properties() called by pid: {os.getpid()}")
         # '(sramBaseAddress=1153202979533225984, dramBaseAddress=1153203082662772736, sramSize=50331648, dramSize=102106132480, tpcEnabledMask=16777215, dramEnabled=1, fd=20, device_id=0, device_type=4)'
-        hpu_properties = torch.hpu.get_device_properties()
-        for prop in hpu_properties.split(", "):
-            if "dramSize" in prop:
-                dramSize=int(prop.replace("dramSize=", ""))
+        # hpu_properties = torch.hpu.get_device_properties()
+        # for prop in hpu_properties.split(", "):
+        #     if "dramSize" in prop:
+        #         dramSize=int(prop.replace("dramSize=", ""))
+        dramSize=102106132480
         return HPUDeviceProperties(dramSize)
 
     def get_device_count(self):
-        return torch.hpu.device_count()
+        log.debug(f"BackendHPU.get_device_count() called by pid: {os.getpid()}")
+        # return torch.hpu.device_count()
+        device_count = int(subprocess.check_output("hl-smi -Q module_id -f csv | wc -l", shell=True, text=True)) - 1
+        return device_count
 
     def set_device(self, device_index : int):
+        log.debug(f"BackendHPU.set_device() called by pid: {os.getpid()}")
+        import habana_frameworks.torch as htorch
         torch.hpu.set_device(device_index)
     
     def get_device(self):
+        log.debug(f"BackendHPU.get_device() called by pid: {os.getpid()}")
+        import habana_frameworks.torch as htorch
         return torch.hpu.current_device()
     
     def device_synchronize(self):
+        log.debug(f"BackendHPU.device_synchronize() called by pid: {os.getpid()}")
+        import habana_frameworks.torch as htorch
         torch.hpu.synchronize()
 
     def empty_cache(self):
+        log.debug(f"BackendHPU.empty_cache() called by pid: {os.getpid()}")
         # empty_cache() is non-op on HPU
         return
 
     def get_dist_module(self):
+        log.debug(f"BackendHPU.get_dist_module() called by pid: {os.getpid()}")
         return dist
 
     def initialize_ccl(self, rank, world_size):
+        log.debug(f"BackendHPU.initialize_ccl() called by pid: {os.getpid()}")
         # from habana_frameworks.torch.distributed.hccl import initialize_distributed_hpu
         # world_size, rank, local_rank = initialize_distributed_hpu()
 
